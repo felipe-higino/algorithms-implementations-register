78d1023c2401ce1c5cee786902bdf33812d33b36
fhps <higino.felipe@hotmail.com>
2020-08-26 20:59:22 -0300

    anonumous type testing

--- CSharp/Anonymous-objects/Program.cs
+++ CSharp/Anonymous-objects/Program.cs
@@ -4,9 +4,31 @@ namespace Anonymous_objects
 {
     class Program
     {
+        
+        public static object generateObj()
+        {
+            return new {name = "menor", age = 12};
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var obj = generateObj();
+            var obj2 = new 
+            {
+                some  = obj,
+                thing = "any",
+                matters = 2,
+                oder = new {
+                    min=1,
+                    max=10,
+                    anin = new { 
+                        hello ="world"
+                    }
+                }
+            };
+
+            
+            Console.WriteLine(generateObj());
         }
     }
 }
